834c54d639e125b67c3ca4cfbf427a8e4316fec8
Sean Huber <sean@shuber.io>
2015-08-28 22:28:31 -0700

    Update branch displaying logic

--- git.tmux
+++ git.tmux
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$CURRENT_DIR/scripts/helpers.sh"
--- scripts/branch.sh
+++ scripts/branch.sh
@@ -5,7 +5,8 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 print_git_branch() {
-  echo "branch"
+  local pwd=$(tmux display-message -p "#{pane_current_command}")
+  echo $pwd
 }
 
 main() {
