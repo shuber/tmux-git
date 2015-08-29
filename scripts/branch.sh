0a1dce68510c77db9d55392808ec6e1a434053b2
Sean Huber <sean@shuber.io>
2015-08-28 22:31:42 -0700

    Try displaying the actual checked out git branch

--- scripts/branch.sh
+++ scripts/branch.sh
@@ -5,8 +5,9 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 print_git_branch() {
-  local working_dir=$(tmux display-message -p "#{pane_current_command}")
-  echo $working_dir
+  local working_dir=$(tmux display-message -p "#{pane_current_path}")
+  local branch=$(cd $working_dir && git branch)
+  echo $branch
 }
 
 main() {
