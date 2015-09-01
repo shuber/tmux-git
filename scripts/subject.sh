84106a69ea2c1729c9b96491c1f93fe13116738a
Sean Huber <sean@shuber.io>
2015-08-31 23:18:29 -0700

    Rename method to fix git_subject error

--- scripts/subject.sh
+++ scripts/subject.sh
@@ -4,7 +4,7 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$CURRENT_DIR/helpers.sh"
 
-print_git_shortsha() {
+print_git_subject() {
   local working_dir=$(tmux display-message -p "#{pane_current_path}")
   local subject=$(cd "$working_dir" && git log -1 --pretty=format:%s)
 
