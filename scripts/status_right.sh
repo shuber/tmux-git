d3d87539988c20115934a64dfc50edee3178dfe7
Sean Huber <sean@shuber.io>
2015-09-05 20:22:16 -0700

    Add whitespace after branch

--- scripts/status_right.sh
+++ scripts/status_right.sh
@@ -11,7 +11,7 @@ print_status_right() {
   local green="#[fg=colour236,bg=colour150]"
   local green_arrow="#[fg=colour150,bg=colour237,nobold,nounderscore,noitalics]"
 
-  local status_right="$green_arrow$green $branch"
+  local status_right="$green_arrow$green $branch "
   local interpolated=$(tmux display-message -p "$status_right")
 
   # "#[fg=colour249,bg=colour237] %Y-%m-%d  %I:%M #[fg=colour4,bg=colour237,nobold,nounderscore,noitalics]#[fg=colour150,bg=colour237,nobold,nounderscore,noitalics]#[fg=colour236,bg=colour150] 18 #[fg=colour236,bg=colour150] + #[fg=colour237,bg=colour150,nobold,nounderscore,noitalics]#[fg=colour131,bg=colour237,nobold,nounderscore,noitalics]#[fg=colour236,bg=colour131] 37 #[fg=colour236,bg=colour131] - #[fg=colour237,bg=colour131,nobold,nounderscore,noitalics]#[fg=colour3,bg=colour237,nobold,nounderscore,noitalics]#[fg=colour236,bg=colour3] #{git_branch}#[fg=colour3,bg=colour3] #()"
