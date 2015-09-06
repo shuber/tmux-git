120b9fc98cbf492e399de24eb041771607c43090
Sean Huber <sean@shuber.io>
2015-09-05 20:34:28 -0700

    Update branch color based on status

--- scripts/status_right.sh
+++ scripts/status_right.sh
@@ -6,12 +6,32 @@ source "$CURRENT_DIR/helpers.sh"
 
 print_status_right() {
   local working_dir=$(tmux display-message -p "#{pane_current_path}")
-  local branch=$(cd "$working_dir" && git symbolic-ref --short HEAD)
+  local branch_name=$(cd "$working_dir" && git symbolic-ref --short HEAD)
 
-  local green="#[fg=colour236,bg=colour150]"
-  local green_arrow="#[fg=colour150,bg=colour237,nobold,nounderscore,noitalics]"
+  local blue="4"
+  local green="150"
+  # local green="2"
+  local red="1"
+  local yellow="3"
 
-  local status_right="$green_arrow$green $branch"
+  local head=$(cd "$working_dir" && git rev-parse @)
+  local remote=$(cd "$working_dir" && git rev-parse @{u})
+  local base=$(cd "$working_dir" && git merge-base @ @{u})
+
+  if [ $head = $remote ]; then # up to date
+    local color=$green
+  elif [ $head = $base ]; then # need to pull
+    local color=$blue
+  elif [ $remote = $base ]; then # need to push
+    local color=$yellow
+  else # diverged
+    local color=$red
+  fi
+
+  local branch="#[fg=colour236,bg=colour$color] $branch_name"
+  local branch_arrow="#[fg=colour$color,bg=colour237,nobold,nounderscore,noitalics]"
+
+  local status_right="$branch_arrow $branch"
   local interpolated=$(tmux display-message -p "$status_right")
 
   # "#[fg=colour249,bg=colour237] %Y-%m-%d  %I:%M #[fg=colour4,bg=colour237,nobold,nounderscore,noitalics]#[fg=colour150,bg=colour237,nobold,nounderscore,noitalics]#[fg=colour236,bg=colour150] 18 #[fg=colour236,bg=colour150] + #[fg=colour237,bg=colour150,nobold,nounderscore,noitalics]#[fg=colour131,bg=colour237,nobold,nounderscore,noitalics]#[fg=colour236,bg=colour131] 37 #[fg=colour236,bg=colour131] - #[fg=colour237,bg=colour131,nobold,nounderscore,noitalics]#[fg=colour3,bg=colour237,nobold,nounderscore,noitalics]#[fg=colour236,bg=colour3] #{git_branch}#[fg=colour3,bg=colour3] #()"
