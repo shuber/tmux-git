e63d22a6cffe4d3b70aee72016fc60343916854a
Sean Huber <sean@shuber.io>
2015-09-05 20:04:24 -0700

    Update status left interpolation

--- scripts/status_left.sh
+++ scripts/status_left.sh
@@ -5,10 +5,16 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$CURRENT_DIR/helpers.sh"
 
 print_status_left() {
-  local working_dir=$(tmux display-message -p "#{pane_current_path}")
-  local status_left=$(cd "$working_dir" && git log -1 --pretty=format:%h)
+  local host=$(tmux display-message -p "#h")
+  local session=$(tmux display-message -p "#S")
 
-  echo $status_left
+  local green="#[fg=colour236,bg=colour150]"
+  local green_arrow="#[fg=colour150,bg=colour237,nobold,nounderscore,noitalics]"
+
+  local status_left="$green $host  $session $green_arrow"
+  local interpolated=$(tmux display-message -p "$status_left")
+
+  echo $interpolated
 }
 
 main() {
