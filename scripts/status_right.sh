d779857ff6e5aa1f4841b8d52a495e38c78196b8
Sean Huber <sean@lendinghome.com>
2016-06-12 22:35:05 -0700

    Display correct staged/unstaged counts in status

--- scripts/status_right.sh
+++ scripts/status_right.sh
@@ -18,12 +18,9 @@ print_status_right() {
   local remote=$(cd "$working_dir" && git rev-parse @{u})
   local base=$(cd "$working_dir" && git merge-base @ @{u})
 
-  local dirty=$(cd "$working_dir" && git diff HEAD --shortstat)
-  local changes=$(echo "$dirty" | perl -pe "s/^.*?(\d+) file.*$/\1/")
-  local insertions=$(echo "$dirty" | perl -pe "s/^.*?(\d+) insertion.*$/\1/")
-  local deletions=$(echo "$dirty" | perl -pe "s/^.*?(\d+) deletion.*$/\1/")
-
-  local staged=$(cd "$working_dir" && git diff --cached --numstat | wc -l)
+  local dirty=$(cd "$working_dir" && git diff --shortstat)
+  local staged=$(cd "$working_dir" && git diff --cached --stat | tail -r | grep -m 1 '.')
+  local staged_files=$(echo "$staged" | perl -pe "s/^.*?(\d+) files? changed.*$/\1/")
 
   if [ "$dirty" ]; then # uncommitted changes
     local color=$yellow
@@ -44,6 +41,9 @@ print_status_right() {
   local spacer="#[fg=colour237,bg=colour1,nobold,nounderscore,noitalics]"
 
   if [ "$dirty" ]; then
+    local changes=$(echo "$dirty" | perl -pe "s/^.*?(\d+) file.*$/\1/")
+    local insertions=$(echo "$dirty" | perl -pe "s/^.*?(\d+) insertion.*$/\1/")
+    local deletions=$(echo "$dirty" | perl -pe "s/^.*?(\d+) deletion.*$/\1/")
     status_right="$branch_arrow$highlight $changes $status_right"
 
     if [ "$deletions" != "$dirty" ]; then
@@ -59,11 +59,11 @@ print_status_right() {
     status_right="$branch_arrow$status_right"
   fi
 
-  if [ "$staged" -ne "0" ]; then
-
-      # draw staged files count in blue
-      # local files=$(echo "$staged" | perl -pe "s/^.*?(\d+) file.*$/\1/")
-      local staged_status="#[fg=colour$blue,bg=colour237,nobold,nounderscore,noitalics]#[fg=colour236,bg=colour$blue] $staged #[fg=colour236,bg=colour$blue] ± #[fg=colour237,bg=colour$blue,nobold,nounderscore,noitalics]"
+  if [ "$staged_files" -ne "0" ]; then
+      local staged_files=$(echo "$staged" | perl -pe "s/^.*?(\d+) files? changed.*$/\1/")
+      local staged_insertions=$(echo "$staged" | perl -pe "s/^.*?(\d+) insertion.*$/\1/")
+      local staged_deletions=$(echo "$staged" | perl -pe "s/^.*?(\d+) deletion.*$/\1/")
+      local staged_status="#[fg=colour$blue,bg=colour237,nobold,nounderscore,noitalics]#[fg=colour236,bg=colour$blue] $staged_files  $staged_insertions+  $staged_deletions- #[fg=colour237,bg=colour$blue,nobold,nounderscore,noitalics]"
       status_right="$staged_status$status_right"
   fi
 
